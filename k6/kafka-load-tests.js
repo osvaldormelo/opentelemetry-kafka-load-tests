b0044d61bbd496562fad7ae92402f2db6544d0c5
Osvaldo Melo <omelo@redhat.com>
2024-05-15 16:19:11 -0300

    Best configs for tests

--- k6/kafka-load-tests.js
+++ k6/kafka-load-tests.js
@@ -5,10 +5,10 @@ export const options = {
     scenarios: {
         my_scenario1: {
             executor: 'constant-arrival-rate',
-            duration: '60s', // total duration
-            preAllocatedVUs: 1000, // to allocate runtime resources
+            duration: '120s', // total duration
+            preAllocatedVUs: 250, // to allocate runtime resources
 
-            rate: 1000, // number of constant iterations given `timeUnit`
+            rate: 500, // number of constant iterations given `timeUnit`
             timeUnit: '1s',
         },
     },
@@ -296,7 +296,7 @@ export default function () {
         }
      });
     const headers = { 'Content-Type': 'text/plain','Accept':'*/*' };
-    const res = http.post('http://camel-quarkus-kafka-api-producer-camel.apps.cluster-r9z76.r9z76.sandbox596.opentlc.com/produce', payload, { headers });
+    const res = http.post('http://camel-quarkus-kafka-api-producer-camel.apps.cluster-pz5k8.pz5k8.sandbox421.opentlc.com/produce', payload, { headers });
 
     check(res, {
         'Post status is 200': (r) => res.status === 200
